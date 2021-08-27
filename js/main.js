f2121539bb2da87c8e2133fe3f28a0ed4e9ac91e
victoria <74865927+avocode1353@users.noreply.github.com>
2021-08-27 21:10:23 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+
+    $('.info__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev">Previous<br><span>Kaktus Plant</span></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next">Next<br><span>Rahasia Plant</span></button>',
+    });
+
+});
